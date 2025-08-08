bbbac711965ddecbcbffb4b97dbad6da686846e3
R.M Dilshan Savinda Rathnayaka <dilshansavinda83@gmail.com>
2025-08-08 23:48:53 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div style={{ display: 'flex' }}>
+    
+      <div style={{ marginLeft: '250px', width: '100%' }}>
+        <Routes>
+      
+        
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
